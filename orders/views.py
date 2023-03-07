35b551079f07a37bc70d9329c5d914443e18e48f
Josiel Borges <josielborges555@gmail.com>
2023-03-07 15:51:33 -0300

    Create order views. Pending.

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class OrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        cart = self.request.user.user_cart
+
+        serializer.save(buyer=self.request.user, cart=cart)
+
+
+class OrderDetailView(generics.RetrieveUpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    lookup_url_kwarg = "order_id"
