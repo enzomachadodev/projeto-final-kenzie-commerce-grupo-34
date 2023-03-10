78e777ad32c96ef6d226c87f0bc3ba2a6dfeda75
Enzo de Freitas Machado <106446549+m4chado@users.noreply.github.com>
2023-03-10 09:08:22 -0300

    Revert "Fix/order views"

--- carts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("cart/<int:product_id>/", views.ProductToCartView.as_view()),
-    path("cart/", views.CartView.as_view()),
-]
